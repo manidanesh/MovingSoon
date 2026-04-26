1bae7be7d59c8b760dbf307933b9847c15285178
manidanesh <mani.danesh@gmail.com>
2026-04-25 18:58:25 -0600

    Initial Commit

---
+++ movingsoon.app/movingsoon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movingsoon_appApp.swift
+//  movingsoon.app
+//
+//  Created by Mani on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct movingsoon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
